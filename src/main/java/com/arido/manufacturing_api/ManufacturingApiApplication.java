3ff023aae88592f1516138e64327c7c8e16ef930
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2026-04-01 18:33:01 -0300

    chore: initial spring boot config and structure from initializr

---
+++ src/main/java/com/arido/manufacturing_api/ManufacturingApiApplication.java
@@ -0,0 +1,13 @@
+package com.arido.manufacturing_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManufacturingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManufacturingApiApplication.class, args);
+	}
+
+}
