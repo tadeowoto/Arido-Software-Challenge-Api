61d31441ee08b0d7b792367a5c0445f3e5c71546
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2026-04-02 15:01:08 -0300

    fix(repo): rename UserSecurityInterface to UserSecurityRepository

--- src/main/java/com/arido/manufacturing_api/repository/UserSecurityRepository.java
+++ src/main/java/com/arido/manufacturing_api/repository/UserSecurityRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface UserSecurityInterface extends JpaRepository<UserSecurity, UserSecurityKey> {
+public interface UserSecurityRepository extends JpaRepository<UserSecurity, UserSecurityKey> {
 
     List<UserSecurity> findByGroupId(Long groupId);
 }
