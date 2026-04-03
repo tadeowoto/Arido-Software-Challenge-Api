f737777a195d640ec72b8f7502bec3839bb1d3eb
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2026-04-03 11:43:53 -0300

    feat(mapper): user mapper added

---
+++ src/main/java/com/arido/manufacturing_api/mapper/UserMapper.java
@@ -0,0 +1,22 @@
+package com.arido.manufacturing_api.mapper;
+
+import com.arido.manufacturing_api.dto.UserDTO;
+import com.arido.manufacturing_api.model.User;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserMapper {
+
+    private final ModelMapper modelMapper;
+
+    public User toEntity(UserDTO userDTO){
+        return modelMapper.map(userDTO, User.class);
+    }
+
+    public UserDTO toDTO(User user){
+        return modelMapper.map(user, UserDTO.class);
+    }
+}
