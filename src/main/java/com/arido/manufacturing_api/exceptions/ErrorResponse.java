6be17eb3a7934b20b35d04d64b099c09cd0237e5
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2026-04-06 12:46:33 -0300

    feat(exceptions): implement global exception handling and fix error response audit

--- src/main/java/com/arido/manufacturing_api/exceptions/ErrorResponse.java
+++ src/main/java/com/arido/manufacturing_api/exceptions/ErrorResponse.java
@@ -1,21 +1,24 @@
 package com.arido.manufacturing_api.exceptions;
 
+import jakarta.persistence.PrePersist;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
-@AllArgsConstructor
+@NoArgsConstructor
 public class ErrorResponse {
     private String message;
     private int statusCode;
-    private LocalDateTime timestamp;
+    private LocalDateTime timestamp = LocalDateTime.now();
     private String errorDetails;
 
-    public ErrorResponse(String errorDetails,  int statusCode, String message) {
+    public ErrorResponse(String errorDetails, int statusCode, String message) {
         this.errorDetails = errorDetails;
         this.statusCode = statusCode;
         this.message = message;
+        this.timestamp = LocalDateTime.now();
     }
 }
