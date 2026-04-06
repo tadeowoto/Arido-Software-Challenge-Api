760a0fa078c8b3797b6a71d19b8d0c23bdbba6a7
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2026-04-06 11:28:53 -0300

    feat(exceptions): Global exception handler methods added

--- src/main/java/com/arido/manufacturing_api/exceptions/ErrorResponse.java
+++ src/main/java/com/arido/manufacturing_api/exceptions/ErrorResponse.java
@@ -1,7 +1,12 @@
 package com.arido.manufacturing_api.exceptions;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 import java.time.LocalDateTime;
 
+@Data
+@AllArgsConstructor
 public class ErrorResponse {
     private String message;
     private int statusCode;
