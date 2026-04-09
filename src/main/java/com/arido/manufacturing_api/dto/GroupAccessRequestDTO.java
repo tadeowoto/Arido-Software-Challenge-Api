a15d831d740568bb6b46af8c462e6732eeb48be6
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2026-04-09 13:57:56 -0300

    feat(UserServiceIMPL): create user with N groups and level access added

--- src/main/java/com/arido/manufacturing_api/dto/GroupAccessRequestDTO.java
+++ src/main/java/com/arido/manufacturing_api/dto/GroupAccessRequestDTO.java
@@ -1,13 +1,12 @@
 package com.arido.manufacturing_api.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 
-@Data
-@AllArgsConstructor
+@Getter
+@Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class GroupAccessRequestDTO {
     private Long groupId;
     private Long accessLevelId;
