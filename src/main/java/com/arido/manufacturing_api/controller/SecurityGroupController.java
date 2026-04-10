502845220d78e9b833982dab1817c21ca0e265a0
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2026-04-10 13:55:44 -0300

    feat(securityGroupController): cors error fixed

--- src/main/java/com/arido/manufacturing_api/controller/SecurityGroupController.java
+++ src/main/java/com/arido/manufacturing_api/controller/SecurityGroupController.java
@@ -5,10 +5,7 @@ import com.arido.manufacturing_api.exceptions.ResourceNotFoundException;
 import com.arido.manufacturing_api.service.SecurityGroupService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,6 +13,7 @@ import java.util.Optional;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/groups")
+@CrossOrigin(origins = "http://localhost:3000")
 public class SecurityGroupController {
 
     private final SecurityGroupService groupService;
